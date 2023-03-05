ef71f1a7e7ba2fd32b7ca4e46533dce28bcdd168
Alena Batushkova <alena.batushkova@gmail.com>
2023-03-05 17:25:10 +0300

    fix header structure, styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import AppBar from '@mui/material/AppBar';
-import Grid from '@mui/material/Grid';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import Avatar from '@mui/material/Avatar';
-import TeamGroup from '../TeamGroup/TeamGroup';
+import AvatarGroup from '@mui/material/AvatarGroup';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import CodeIcon from '@mui/icons-material/Code';
+import Layout from '../Layout/Layout';
+
+const headerAvatar = {
+  width: {
+    md: 54,
+    xs: 44,
+  },
+  height: {
+    md: 54,
+    xs: 44,
+  },
+};
 
 interface IHeaderProps {
   onDrawerToggle: () => void;
@@ -38,7 +52,30 @@ export default function Header(props: IHeaderProps) {
           </Grid>
         </Toolbar>
       </AppBar>
-      <TeamGroup />
+      <Layout pt={3} pb={3}>
+        <Grid
+          container
+          spacing={{ md: 2, xs: 1}}
+          alignItems="center"
+          direction={{ md: 'row', xs: 'column' }}
+        >
+          <Grid item>
+            <AvatarGroup>
+              <Avatar src="" alt="Vlad" sx={{ ...headerAvatar }} />
+              <Avatar src="" alt="Alon" sx={{ ...headerAvatar }} />
+            </AvatarGroup>
+          </Grid>
+          <Grid item sx={{display: 'flex', alignItems: 'center', justifyContent: 'center', flexWrap: 'wrap' }}>
+            <Typography variant="h5" component="p">
+              Vlad
+            </Typography>
+            <CodeIcon sx={{ fontSize: 32, mx: 1 }} />
+            <Typography variant="h5" component="p">
+              Alon
+            </Typography>
+          </Grid>
+        </Grid>
+      </Layout>
     </>
   );
 }
