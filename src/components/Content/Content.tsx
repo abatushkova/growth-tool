95d8824a9ca4c49aa7aa1230ec651f1806f9e0ef
Alena Batushkova <alena.batushkova@gmail.com>
2023-03-17 22:01:50 +0300

    add topic-page to content

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import Layout from '../Layout/Layout';
 import MeetingPage from '../MeetingPage/MeetingPage';
+import TopicPage from '../TopicPage/TopicPage';
 
 export default function Content() {
   return (
-    <Layout pb={6}>
+    <>
       <MeetingPage />
-    </Layout>
+      <TopicPage />
+    </>
   );
 }
---
+++ src/components/TopicPage/TopicPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Typography from '@mui/material/Typography';
+import Layout from '../Layout/Layout';
+
+export default function TopicPage() {
+  return (
+    <Layout>
+      <Typography variant="h5" component="h2" sx={{ my: 5 }} align="left">
+        Topic 1
+      </Typography>
+    </Layout>
+  )
+}
