bcad979984806f69c5c2b5be1a24efa84ae86f9e
Alena Batushkova <alena.batushkova@gmail.com>
2023-03-17 19:06:07 +0300

    add menu to header avatar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,85 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 import Toolbar from '@mui/material/Toolbar';
 import Avatar from '@mui/material/Avatar';
-import AvatarGroup from '@mui/material/AvatarGroup';
 import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import CodeIcon from '@mui/icons-material/Code';
-import Layout from '../Layout/Layout';
-
-const headerAvatar = {
-  width: {
-    md: 54,
-    xs: 44,
-  },
-  height: {
-    md: 54,
-    xs: 44,
-  },
-};
 
 interface IHeaderProps {
   onDrawerToggle: () => void;
 }
 
 export default function Header(props: IHeaderProps) {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
   const { onDrawerToggle } = props;
 
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <>
-      <AppBar position="sticky" elevation={0}>
-        <Toolbar>
-          <Grid container spacing={1} alignItems="center">
-            <Grid item sx={{ display: { md: 'none', xs: 'block' } }}>
-              <IconButton
-                aria-label="open drawer"
-                onClick={onDrawerToggle}
-                edge="start"
-              >
-                <MenuIcon />
-              </IconButton>
-            </Grid>
-            <Grid item xs />
-            <Grid item>
-              <IconButton sx={{ p: 0.5 }}>
-                <Avatar src="" alt="My Avatar" />
-              </IconButton>
-            </Grid>
+    <AppBar position="sticky" elevation={0}>
+      <Toolbar>
+        <Grid container spacing={1} alignItems="center">
+          <Grid item sx={{ display: { md: 'none', xs: 'block' } }}>
+            <IconButton
+              aria-label="open drawer"
+              onClick={onDrawerToggle}
+              edge="start"
+            >
+              <MenuIcon />
+            </IconButton>
           </Grid>
-        </Toolbar>
-      </AppBar>
-      <Layout pt={3} pb={3}>
-        <Grid
-          container
-          spacing={{ md: 2, xs: 1}}
-          alignItems="center"
-          direction={{ md: 'row', xs: 'column' }}
-        >
+          <Grid item xs />
           <Grid item>
-            <AvatarGroup>
-              <Avatar src="" alt="Vlad" sx={{ ...headerAvatar }} />
-              <Avatar src="" alt="Alon" sx={{ ...headerAvatar }} />
-            </AvatarGroup>
-          </Grid>
-          <Grid item sx={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            flexWrap: 'wrap'
-          }}>
-            <Typography variant="h5" component="p">
-              Vlad
-            </Typography>
-            <CodeIcon sx={{ fontSize: 32, mx: 1 }} />
-            <Typography variant="h5" component="p">
-              Alon
-            </Typography>
+            <IconButton
+              id="basic-button"
+              aria-controls={open ? 'basic-menu' : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? 'true' : undefined}
+              onClick={handleClick}
+              sx={{ p: 0.5 }}
+            >
+              <Avatar src="" alt="My avatar" />
+            </IconButton>
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{
+                'aria-labelledby': 'basic-button',
+              }}
+            >
+              <MenuItem onClick={handleClose}>My account</MenuItem>
+              <MenuItem onClick={handleClose}>Logout</MenuItem>
+            </Menu>
           </Grid>
         </Grid>
-      </Layout>
-    </>
+      </Toolbar>
+    </AppBar>
   );
 }
