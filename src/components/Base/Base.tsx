138fcd31d5cfaa49f0bdd4899f83a66aa2abf484
Alena Batushkova <alena.batushkova@gmail.com>
2023-03-17 22:03:02 +0300

    add textfield custom style, remove divider

--- src/components/Base/Base.tsx
+++ src/components/Base/Base.tsx
@@ -3,7 +3,6 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
-import Divider from '@mui/material/Divider';
 import Aside from '../Aside/Aside';
 import Content from '../Content/Content';
 import Header from '../Header/Header';
@@ -95,6 +94,24 @@ theme = {
         },
       },
     },
+    // MuiTextField: {
+    //   styleOverrides: {
+    //     root: {
+    //       '& .MuiInputBase-multiline': {
+    //         padding: `0 ${theme.spacing(1)}`,
+    //         marginLeft: `-${theme.spacing(1)}`,
+    //         fontSize: 20,
+    //         fontWeight: theme.typography.fontWeightMedium,
+    //         '&:hover': {
+    //           outline: `1px solid rgba(51,51,51,0.06)`,
+    //         },
+    //       },
+    //       "& .Mui-focused": {
+    //         backgroundColor: theme.palette.white.main,
+    //       }
+    //     },
+    //   }
+    // },
     MuiTabs: {
       styleOverrides: {
         indicator: {
@@ -115,13 +132,6 @@ theme = {
         },
       },
     },
-    MuiTooltip: {
-      styleOverrides: {
-        tooltip: {
-          borderRadius: 4,
-        },
-      },
-    },
     MuiListItemButton: {
       styleOverrides: {
         root: {
@@ -130,7 +140,7 @@ theme = {
             backgroundColor: 'rgba(0,155,229,0.12)',
           },
           '&:hover': {
-            backgroundColor: 'rgb(51,51,51,0.06)',
+            backgroundColor: 'rgba(51,51,51,0.06)',
           }
         },
       },
@@ -187,7 +197,6 @@ export default function Base() {
         </Box>
         <Box sx={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
           <Header onDrawerToggle={handleDrawerToggle} />
-          <Divider />
           <Box component="main" sx={{ flex: 1 }}>
             <Content />
           </Box>
