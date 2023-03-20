bafffd55619757a8ffb24b3bb70df54e92bf5800
Alena Batushkova <alena.batushkova@gmail.com>
2023-03-20 22:04:06 +0300

    add action icons, text input to meeting-item

--- src/components/MeetingItem/MeetingItem.tsx
+++ src/components/MeetingItem/MeetingItem.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { styled } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
 import MuiPaper, { PaperProps } from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
@@ -9,13 +10,26 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Chip from '@mui/material/Chip';
 import Stack from '@mui/material/Stack';
-import { styled } from '@mui/material/styles';
+import Tooltip from '@mui/material/Tooltip';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import AddIcon from '@mui/icons-material/Add';
+import AddLinkIcon from '@mui/icons-material/AddLink';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Paper from '@mui/material/Paper';
+import TextField from '@mui/material/TextField';
+import { meetings } from '../../store/fakeMeetings';
+
+const ITEM_HEIGHT = 48;
 
 const Item = styled((props: PaperProps) => (
   <MuiPaper {...props} />
 ))(({ theme }) => ({
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
+  ...theme.typography.body1,
+  paddingInline: theme.spacing(2),
+  paddingBlock: theme.spacing(0.5),
 }));
 
 const CustomAccordion = styled((props: AccordionProps) => (
@@ -25,25 +39,44 @@ const CustomAccordion = styled((props: AccordionProps) => (
 }));
 
 interface ITopic {
-  name: string;
+  topicId: number;
+  title: string;
+}
+interface IGuest {
+  guestId: number;
 }
 interface IMeetingProps {
-  id: number;
+  meetingId: number;
   title: string;
-  created: string;
+  createdAt: string;
+  closedAt: string;
+  plannedAt: string;
+  ownerId: number;
+  guests: IGuest[];
   topics: ITopic[];
 }
 
-export default function MeetingItem({ id, title, created, topics }: IMeetingProps) {
+export default function MeetingItem(props: IMeetingProps) {
+  const { meetingId, title, createdAt, topics, closedAt } = props;
   const [expanded, setExpanded] = useState<number | false>(false);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   const toggleAccordion =
-    (id: number) => (event: React.SyntheticEvent, isExpanded: boolean) => {
-      setExpanded(isExpanded ? id : false);
+    (meetingId: number) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+      setExpanded(isExpanded ? meetingId : false);
     };
 
   return (
-    <CustomAccordion expanded={expanded === id} onChange={toggleAccordion(id)}>
+    <CustomAccordion expanded={expanded === meetingId} onChange={toggleAccordion(meetingId)}>
       <AccordionSummary
         expandIcon={<ExpandMoreIcon />}
         aria-controls="panel1bh-content"
@@ -51,10 +84,12 @@ export default function MeetingItem({ id, title, created, topics }: IMeetingProp
       >
         <Grid container spacing={1}>
           <Grid item xs={12} container flexWrap="nowrap" spacing={2}>
-            <Grid item>
-              <Chip label={topics.length} size="small" />
-            </Grid>
-            <Grid item>
+            {topics.length > 0 && (
+              <Grid item>
+                <Chip label={topics.length} size="small" />
+              </Grid>
+            )}
+            <Grid item container spacing={2}>
             {/* <Grid item xs={11}>
               <Box
                 component="form"
@@ -71,23 +106,127 @@ export default function MeetingItem({ id, title, created, topics }: IMeetingProp
                   sx={{ fontSize: '20px' }}
                 />
               </Box> */}
-              <Typography variant="h6" component="p" color="text.main">{title}</Typography>
+              <Grid item>
+                <Typography variant="h6" component="p" color="text.main">
+                  {title}
+                </Typography>
+              </Grid>
+              {/* <Grid item>
+                <Chip
+                  variant="outlined"
+                  color={ !closedAt ? 'primary': 'success' }
+                  size="small"
+                  label={ !closedAt ? 'upcoming': 'passed' }
+                />
+              </Grid> */}
             </Grid>
           </Grid>
           <Grid item xs={12}>
             <Typography variant="body2" color="text.secondary">
-              {created}
+              {createdAt}
             </Typography>
           </Grid>
         </Grid>
       </AccordionSummary>
       <AccordionDetails>
-        <Stack spacing={0.5}>
-          {topics.map((topic, index) => (
-            <Item key={index}>{topic.name}</Item>
-          ))}
-        </Stack>
-        <Button variant="text" sx={{ mt: 2 }}>+ Create Topic</Button>
+        {topics.length > 0 && (
+          <Stack spacing={0.5} sx={{ mb: 2 }}>
+            {topics.map((topic) => (
+              <Item key={topic.topicId}>
+                <Grid container spacing={1} alignItems="center">
+                  <Grid item xs>
+                    {topic.title}
+                  </Grid>
+                  <Grid item>
+                    <Tooltip title="Carry-over">
+                      {/* <IconButton size="small">
+                        <AddLinkIcon fontSize="inherit" />
+                      </IconButton> */}
+                      <IconButton
+                        size="small"
+                        aria-label="more"
+                        id="long-button"
+                        aria-controls={open ? 'long-menu' : undefined}
+                        aria-expanded={open ? 'true' : undefined}
+                        aria-haspopup="true"
+                        onClick={handleClick}
+                      >
+                        <AddLinkIcon fontSize="inherit" />
+                      </IconButton>
+                    </Tooltip>
+                    <Menu
+                      id="long-menu"
+                      MenuListProps={{
+                        'aria-labelledby': 'long-button',
+                      }}
+                      anchorEl={anchorEl}
+                      open={open}
+                      onClose={handleClose}
+                      PaperProps={{
+                        style: {
+                          maxHeight: ITEM_HEIGHT * 4.5,
+                        },
+                      }}
+                    >
+                      {meetings.map((meeting) => (
+                        <MenuItem
+                          key={meeting.meetingId}
+                          selected={meeting.meetingId === 2}
+                          onClick={handleClose}
+                        >
+                          {meeting.createdAt}
+                        </MenuItem>
+                      ))}
+                    </Menu>
+                  </Grid>
+                  <Grid item>
+                    <Tooltip title="Edit">
+                      <IconButton size="small">
+                        <EditIcon fontSize="inherit" />
+                      </IconButton>
+                    </Tooltip>
+                  </Grid>
+                  <Grid item>
+                    <Tooltip title="Delete">
+                      <IconButton size="small">
+                        <DeleteIcon fontSize="inherit" />
+                      </IconButton>
+                    </Tooltip>
+                  </Grid>
+                </Grid>
+              </Item>
+            ))}
+          </Stack>
+        )}
+        {/* <Button variant="text" startIcon={<AddIcon />}>
+          Add Topic
+        </Button> */}
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              variant="outlined"
+              id="outlined-question"
+              label="Topic"
+              multiline
+              fullWidth
+              size="small"
+              sx={{ backgroundColor: 'white' }}
+            />
+          </Grid>
+          <Grid item container spacing={1}>
+            <Grid item>
+              <Button variant="contained">
+                Add
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button variant="text">
+                Cancel
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+
       </AccordionDetails>
     </CustomAccordion>
   );
