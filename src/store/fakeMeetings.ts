28ce29491ed58705236ce462bfff360a31ef127e
Alena Batushkova <alena.batushkova@gmail.com>
2023-03-20 21:59:40 +0300

    update fake-meetings store

--- src/store/fakeMeetings.ts
+++ src/store/fakeMeetings.ts
@@ -1,31 +1,127 @@
 export const meetings = [
   {
-    id: 1,
+    meetingId: 3,
     title: 'New Meeting',
-    created: 'Dec 31, 2022',
+    createdAt: 'Dec 31, 2022',
+    closedAt: '',
+    plannedAt: '',
+    ownerId: 1,
+    guests: [
+      {
+        guestId: 1,
+      },
+    ],
     topics: [
       {
-        name: 'Topic 1',
+        topicId: 1,
+        title: 'Topic 1',
+        createdAt: '',
+        categories: [
+          'NPS',
+          'GrowthPlan',
+          '101',
+        ],
+        comments: [
+          {
+            commentId: 1,
+            createdAt: '',
+            formView: 'Comment',
+            comment: 'Useful information',
+            question: '',
+            score: 0,
+            meetingId: 3,
+          },
+        ],
       },
       {
-        name: 'Topic 2',
-      },
-      {
-        name: 'Topic 3',
+        topicId: 3,
+        title: 'Topic 3',
+        createdAt: '',
+        categories: [
+          'GrowthPlan',
+        ],
+        comments: [
+          {
+            commentId: 3,
+            createdAt: '',
+            formView: 'Score',
+            comment: 'Useful information',
+            question: '',
+            score: 5,
+            meetingId: 3,
+          },
+        ],
       },
     ]
   },
   {
-    id: 2,
+    meetingId: 2,
     title: 'New Meeting',
-    created: 'Dec 16, 2022',
+    createdAt: 'Dec 16, 2022',
+    closedAt: 'Dec 20, 2022',
+    plannedAt: '',
+    ownerId: 1,
+    guests: [
+      {
+        guestId: 1,
+      },
+    ],
     topics: [
       {
-        name: 'Topic 1',
+        topicId: 1,
+        title: 'Topic 1',
+        createdAt: '',
+        categories: [
+          'NPS',
+          'GrowthPlan',
+          '101',
+        ],
+        comments: [
+          {
+            commentId: 4,
+            createdAt: '',
+            formView: 'Score',
+            comment: 'Useful information',
+            question: '',
+            score: 8,
+            meetingId: 2,
+          },
+        ],
       },
       {
-        name: 'Topic 2',
+        topicId: 2,
+        title: 'Topic 2',
+        createdAt: '',
+        categories: [
+          '101',
+        ],
+        comments: [
+          {
+            commentId: 5,
+            createdAt: '',
+            formView: 'QA',
+            comment: 'Useful answer',
+            question: 'Question',
+            score: 0,
+            meetingId: 2,
+          },
+        ],
       },
-    ]
+    ],
+  },
+  {
+    meetingId: 1,
+    title: 'New Meeting',
+    createdAt: 'Nov 25, 2022',
+    closedAt: 'Dec 20, 2022',
+    plannedAt: '',
+    ownerId: 1,
+    guests: [
+      {
+        guestId: 1,
+      },
+    ],
+    topics: [
+    ],
   },
 ];
