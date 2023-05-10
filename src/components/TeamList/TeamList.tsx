16735664d8f76e8f0ee9d92b72df545fc47c7035
Alena Batushkova <alena.batushkova@gmail.com>
2023-05-10 22:02:59 +0300

    connect persons-data from localStorage to team-list component

--- src/components/TeamList/TeamList.tsx
+++ src/components/TeamList/TeamList.tsx
@@ -1,52 +1,39 @@
 import React, { useState } from 'react';
 import {
   List,
-  ListItem,
-  ListItemButton,
-  ListItemText,
-  ListItemAvatar,
-  Avatar,
   Box,
-  Stack,
   TextField,
   InputAdornment,
-  IconButton,
 } from '@mui/material';
-import PersonAddIcon from '@mui/icons-material/PersonAdd';
 import SearchIcon from '@mui/icons-material/Search';
-import CloseIcon from '@mui/icons-material/Close';
-import SaveIcon from '@mui/icons-material/Save';
 import TeamItem from '../TeamItem/TeamItem';
-import { members } from '../../store/fakeMembers';
+import TeamItemAdd from '../TeamItemAdd/TeamItemAdd';
+import { selectPersons } from '../../features/persons/personsSlice';
+import { useTypedSelector } from '../../app/hooks';
 
 export default function TeamList() {
   const [selectedMember, setSelectedMember] = useState(-1);
-  const [isAdding, setIsAdding] = useState(false);
+  const members = useTypedSelector(selectPersons);
+  const sortedMembers = [...members].sort((a, b) => (
+    (a.personName > b.personName) ? 1 : -1
+  ));
 
   const handleMemberSelect = (
-    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>,
     index: number,
   ) => {
     setSelectedMember(index);
   };
 
-  const handleAddOpen = () => setIsAdding(true);
-  const handleAddClose = () => setIsAdding(false);
-
   return (
     <>
       <Box sx={{ p: 2 }}>
-        <Box
-          component="form"
-          noValidate
-          autoComplete="off"
-        >
+        <Box component="form" noValidate autoComplete="off">
           <TextField
             id="members-search"
             type="search"
             placeholder="Search"
-            size="small"
-            fullWidth
+            size="small" fullWidth
             InputProps={{
               startAdornment: (
                 <InputAdornment position="start">
@@ -66,45 +53,11 @@ export default function TeamList() {
         bottom: 0,
         overflow: 'auto',
       }}>
-        {isAdding ? (
-          <Stack direction="row" alignItems="center" spacing={0.5} sx={{ pt: 0.75, pr: 1.5, pb: 1, pl: 2 }}>
-            <TextField
-              id="member-add"
-              variant="standard"
-              placeholder="Name"
-              size="small"
-              fullWidth
-              InputProps={{
-                style: { fontSize: 14 }
-              }}
-            />
-            <IconButton aria-label="Add" size="small">
-              <SaveIcon fontSize="inherit" />
-            </IconButton>
-            <IconButton
-              aria-label="Cancel"
-              size="small"
-              onClick={handleAddClose}
-            >
-              <CloseIcon fontSize="inherit" />
-            </IconButton>
-          </Stack>
-        ) : (
-          <ListItem disablePadding>
-            <ListItemButton onClick={handleAddOpen}>
-              <ListItemAvatar>
-                <Avatar>
-                  <PersonAddIcon sx={{ fontSize: 20 }} />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText>Add member</ListItemText>
-            </ListItemButton>
-          </ListItem>
-        )}
-        {/* sorted list A-Z */}
-        {members.map((member) => (
+        <TeamItemAdd />
+        {sortedMembers.map((member, index) => (
           <TeamItem
             key={member.personId}
+            index={index}
             {...member}
             selected={selectedMember}
             onMemberClick={handleMemberSelect}
