01abcfebf4e07eaa020b324018f40a2fc5285961
Alena Batushkova <alena.batushkova@gmail.com>
2023-05-16 16:54:23 +0300

    remove unused styled-provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { StyledEngineProvider } from '@mui/material/styles';
 import { Provider } from 'react-redux';
 import { debounce } from 'debounce';
 import { saveState } from './app/localStorage';
@@ -17,10 +16,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <StyledEngineProvider injectFirst>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </StyledEngineProvider>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
