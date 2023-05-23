878104ba7ab3974a35a00a6432dee0f002c53c4c
Alena Batushkova <alena.batushkova@gmail.com>
2023-05-24 00:44:31 +0300

    add app-select to hooks

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,5 +1,5 @@
-import { TypedUseSelectorHook, useSelector } from "react-redux";
-import type { RootState } from "./store";
+import { TypedUseSelectorHook, useSelector, useDispatch } from "react-redux";
+import type { RootState, AppDispatch } from "./store";
 
-// Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
