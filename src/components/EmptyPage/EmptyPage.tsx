8531f2a1fc3f3d933dfd5c9b54c70a0760c5719a
Alena Batushkova <alena.batushkova@gmail.com>
2023-05-31 20:51:11 +0300

    add empty-page component, condition with no-active-guest to main component

---
+++ src/components/EmptyPage/EmptyPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+import Layout from '../Layout/Layout';
+
+export default function EmptyPage() {
+  return (
+    <Layout>
+      <Typography variant="h3">
+        Select guest to create a Meeting.
+      </Typography>
+    </Layout>
+  )
+}
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -3,12 +3,23 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import MeetingPage from '../MeetingPage/MeetingPage';
 import TopicPage from '../TopicPage/TopicPage';
+import EmptyPage from '../EmptyPage/EmptyPage';
+import { useAppSelector } from '../../app/hooks';
+import { selectPerson } from '../../features/persons/personsSlice';
 
 export default function Main() {
+  const selectedPerson = useAppSelector(selectPerson);
+
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <MeetingPage />
-      <TopicPage />
+      {selectedPerson.personId ? (
+        <>
+          <MeetingPage />
+          <TopicPage />
+        </>
+      ) : (
+        <EmptyPage />
+      )}
     </LocalizationProvider>
   );
 }
